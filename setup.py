da45054217b888de3cca1f3ab3f095fa1c2f5502
Andrew Holmes <andrewholmes011002@gmail.com>
2023-10-28 21:19:22 -0400

    setting up as lib

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dnn",
+    version="0.1.0",
+    author="Andrew Holmes",
+    author_email="andrewholmes011002@gmail.com",
+    url="https://github.com/Andrew011002/Deep-Neural-Networks",
+    packages=find_packages("dnn"),
+    python_requires=">=3.6",
+)
