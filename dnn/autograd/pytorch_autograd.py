418bd49e7d25fae26f9864d987a3d01eecf6192c
Andrew Holmes <andrewholmes011002@gmail.com>
2023-11-01 13:01:48 -0400

    basic example of using the features of autograd in PyTorch

--- dnn/autograd/pytorch_autograd.py
+++ dnn/autograd/pytorch_autograd.py
@@ -1,45 +1,44 @@
 import torch
 import torch.nn as nn
+import numpy as np
 
 
 def predict(x: torch.Tensor, w: torch.Tensor, b: torch.Tensor):
-    z = torch.matmul(x, w) + b
-    a = torch.sigmoid(z)
-    return a
+    z = x * w + b
+    return z
 
 
 def main():
 
-    n_features, m = 100, 10000
-    x = torch.randn(m, n_features)
-    y = torch.randint(0, 2, (m, 1)).float()
+    x = torch.arange(-10, 10).unsqueeze(0).float()
+    m = 3
+    a = 5
+    y = m * x + a  # func
 
-    w = torch.randn(n_features, 1)
-    w.requires_grad_()
-    b = torch.zeros(1, requires_grad=True)
+    # params
+    w = torch.rand((1, 1), requires_grad=True)
+    b = torch.zeros((1,), requires_grad=True)
 
+    learning_rate = 1e-2
     iters = 1000
-    learning_rate = 1e-1
-    print_every = 250
 
-    for i in range(iters):
+    for iter in range(iters):
+
         out = predict(x, w, b)
-        loss = nn.functional.binary_cross_entropy(out, y)
+        loss = torch.nn.functional.mse_loss(out, y)
         loss.backward()
 
-        # grads acucmulated so just need to update params w/ their grads
         with torch.no_grad():
-            w.data -= w.grad * learning_rate  # Removed division by m
-            b.data -= b.grad * learning_rate
+            w.data = w.data - w.grad * learning_rate
+            b.data = b.data - b.grad * learning_rate
 
-        if (i + 1) % print_every == 0 and i != 1:
-            # print(w.grad.tolist())
-            # print(b.grad.tolist())
-            print(f"iter: {i + 1}: {loss:.4f}")
-
-        # zero grads to prevent old grads being apart of backprop step for next iter
         w.grad.zero_()
         b.grad.zero_()
+
+    assert np.isclose(
+        w.item(), m, rtol=1e-3), "incorrect weight learned"
+    assert np.isclose(
+        b.item(), a, rtol=1e-3), "incorret bias learned"
 
 
 if __name__ == "__main__":
