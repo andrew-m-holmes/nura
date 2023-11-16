e17157655cc3693a9bfbc27568f72ed3b70e6dcf
Andrew <andrewholmes011002@gmail.com>
2023-11-15 20:42:56 -0500

    hack for circular import

--- tests/grad_check.py
+++ tests/grad_check.py
@@ -1,5 +1,5 @@
+import deepnet
 import deepnet.nn.functional as f
-from deepnet.tensor import tensor
 
 
 def test_add():
@@ -12,17 +12,17 @@ def test_mul():
 
 def main():
 
-    a = tensor(3, use_grad=True)
-    b = tensor(4, use_grad=True)
+    a = deepnet.tensor(3, use_grad=True)
+    b = deepnet.tensor(4, use_grad=True)
     c = a * b
 
     print(c)
     c.backward()
     print(a.grad, b.grad)
-    print(tensor(1, use_grad=1))
+    print(deepnet.tensor(1, use_grad=1))
 
-    assert a.grad == b.data
-    assert b.grad == a.data
+    assert a.grad.data == b.data
+    assert b.grad.data == a.data
 
 
 if __name__ == "__main__":
