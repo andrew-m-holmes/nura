199a6df8eb95817e1cce9aa8631921a85603b690
andmholm <andmholm@iu.edu>
2023-11-25 20:01:18 -0500

    forward grad exposed in main init file

--- deepnet/__init__.py
+++ deepnet/__init__.py
@@ -1,4 +1,7 @@
-from .autograd.tensor import tensor, Tensor
-from .autograd.mode import no_grad, use_grad, set_grad
+from ._tensor import Tensor, tensor
+from .autograd.mode import no_grad, use_grad, set_grad, forward_autograd
+from .utils import zeros, zeros_like, ones, ones_like, randn, rand, randint
 
-__all__ = ["tensor", "Tensor", "no_grad", "use_grad", "set_grad"]
+__all__ = ["tensor", "Tensor", "no_grad", "use_grad", "set_grad",
+           "forward_autograd", "zeros", "zeros_like", "ones", "ones_like",
+           "randn", "rand", "randint"]
