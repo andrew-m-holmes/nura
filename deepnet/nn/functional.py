922d3f0d67a9f2bff2009a10e29fcee3d4db4a0a
andmholm <andmholm@iu.edu>
2023-11-25 21:39:32 -0500

    deleted helper because it was moved and now calling tensor function so errors are raise for invalid args passed for the Tensor class

--- deepnet/nn/functional.py
+++ deepnet/nn/functional.py
@@ -1,60 +1,41 @@
-import deepnet
 from .primitives import *
 
 
 def add(a, b):
-    a, b = _format_to_tensor(a, b)
     out = Add.apply(a, b)
     return out
 
 
 def sub(a, b):
-    a, b = _format_to_tensor(a, b)
     out = Sub.apply(a, b)
     return out
 
 
 def mul(a, b):
-    a, b = _format_to_tensor(a, b)
     out = Mul.apply(a, b)
     return out
 
 
 def div(a, b):
-    a, b = _format_to_tensor(a, b)
     out = Div.apply(a, b)
     return out
 
 
 def matmul(a, b):
-    a, b = _format_to_tensor(a, b)
     out = Matmul.apply(a, b)
     return out
 
 
 def pow(a, b):
-    a, b = _format_to_tensor(a, b)
     out = Pow.apply(a, b)
     return out
 
 
 def tranpose(a, dim_0, dim_1):
-    a = _format_to_tensor(a)[0]
     out = Tranpose.apply(a, dim_0, dim_1)
     return out
 
 
 def clone(a):
-    a = _format_to_tensor(a)[0]
     out = Clone.apply(a)
     return out
-
-
-def _format_to_tensor(*inputs):
-    tensors = []
-    for input in inputs:
-        if not isinstance(input, Tensor):
-            tensors.append(deepnet.tensor(input))
-        else:
-            tensors.append(input)
-    return tensors
