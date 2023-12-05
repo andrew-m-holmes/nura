b2d1c5a003c032c8caa78ef8ba7f4e5020da155b
Andrew <andrewholmes011002@gmail.com>
2023-12-04 23:21:04 -0500

    updated imports

--- deepnet/__init__.py
+++ deepnet/__init__.py
@@ -1,6 +1,6 @@
 from .tensors import Tensor, DualTensor, tensor, dual_tensor
 from .autograd.mode import no_grad, use_grad, set_grad, forward_ad
-from .utils import zeros, zeros_like, ones, ones_like, randn, rand, randint, is_tensor, is_dual_tensor
+from .utils import zeros, zeros_like, ones, ones_like, randn, rand, randint, identity, is_tensor, is_dual_tensor
 from .dtype import dtype, byte, char, short, int, long, half, float, double, bool
 
 
@@ -8,5 +8,6 @@ __all__ = [
     "tensor", "Tensor", "DualTensor", "tensor", "dual_tensor",
     "no_grad", "use_grad", "set_grad", "forward_ad", "zeros",
     "zeros_like", "ones", "ones_like", "randn", "rand", "randint",
-    "is_tensor", "is_dual_tensor", "dtype", "byte", "char", "short",
-    "int", "long", "half", "float", "double", "bool"]
+    "identity", "is_tensor", "is_dual_tensor", "dtype", "byte",
+    "char", "short", "int", "long", "half", "float", "double",
+    "bool"]
