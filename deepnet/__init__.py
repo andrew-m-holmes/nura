9e8c6368b1e6cdb3f0d9cb2963a2bf1722bc3eca
Andrew <andrewholmes011002@gmail.com>
2023-12-10 00:19:23 -0500

    added autograd.mode funcs to main dunder init

--- deepnet/__init__.py
+++ deepnet/__init__.py
@@ -1,13 +1,15 @@
 from .tensors import Tensor, DualTensor, tensor, dual_tensor
-from .autograd.mode import no_grad, use_grad, set_grad, forward_ad
+from .autograd.mode import (
+    grad_enabled, forward_ad_enabled, no_grad, use_grad, set_grad,
+    forward_ad)
 from .dtype import *
 from .utils import *
 
 
-__all__ = [
-    "Tensor", "DualTensor", "tensor", "dual_tensor",
-    "no_grad", "use_grad", "set_grad", "forward_ad", "zeros",
-    "zeros_like", "ones", "ones_like", "randn", "rand", "randint",
-    "identity", "full", "is_tensor", "is_dual_tensor", "dtype",
-    "byte", "char", "short", "int", "long", "half", "float",
-    "double", "bool", "typename"]
+__all__ = ["Tensor", "DualTensor", "tensor", "dual_tensor",
+           "grad_enabled", "forward_ad_enabled", "no_grad",
+           "use_grad", "set_grad", "forward_ad", "zeros",
+           "zeros_like", "ones", "ones_like", "randn", "rand",
+           "randint", "identity", "full", "is_tensor",
+           "is_dual_tensor", "dtype", "byte", "char", "short", "int",
+           "long", "half", "float", "double", "bool", "typename"]
