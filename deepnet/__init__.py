b2f649dfd202a3feeb599115cebe5f4e1dad0764
Andrew <andrewholmes011002@gmail.com>
2023-12-14 19:45:32 -0500

    changed imports

--- deepnet/__init__.py
+++ deepnet/__init__.py
@@ -1,7 +1,5 @@
-from .tensors import Tensor, DualTensor, tensor, dual_tensor
-from .autograd.mode import (
-    grad_enabled, forward_ad_enabled, no_grad, use_grad, set_grad,
-    forward_ad)
+from .tensors import *
+from .autograd.mode import *
 from .dtype import *
 from .utils import *
 from .functional import *
