365b5fedc5318fe76f7e504d7db0006f665e0d9f
andmholm <andmholm@iu.edu>
2023-12-24 19:08:23 -0500

    testing things

--- tests/experimental.py
+++ tests/experimental.py
@@ -1,15 +1,26 @@
+import torch
+import torch.autograd.forward_ad as fwad
+import torch.autograd.functional as f
 import numpy as np
 import deepnet
-import deepnet.functional as f
-from deepnet.autograd.functional import vjp, jvp
 
 def main():
+
+    a = torch.rand((1,)).float()
+    b = torch.rand((1,)).float()
+    v = torch.ones((1,)).float()
+
+    output, jvp = f.jvp(torch.mul, (a, b), (v, v))
+    print(output, jvp)
     
-    a = np.random.rand(4, 5, 6)
-    b = a.sum()
-    c = np.random.rand(1)
-    d = np.broadcast_to(b, (1,))
-    print(d.shape)
+    with deepnet.forward_ad():
+        a = deepnet.rand().float()
+        b = deepnet.rand().float()
+        a.dual(inplace=True)
+        b.dual(inplace=True)
+        c = deepnet.mul(a, b)
+        print(c.tangent) 
+
 
 if __name__ == "__main__":
     main()
