ce70995ea394e1e08bb0f7ac220edffd0e1e087b
andmholm <andmholm@iu.edu>
2023-12-26 20:22:31 -0500

    defautling to 4

--- deepnet/__init__.py
+++ deepnet/__init__.py
@@ -3,3 +3,5 @@ from .autograd.mode import *
 from .dtype import *
 from .utils import *
 from .functional import *
+
+np.set_printoptions(precision=4)
---
+++ deepnet/autograd/__init__.py
@@ -0,0 +1,2 @@
+import numpy as np
+np.set_printoptions(precision=4)
