c32f3bf288a167c9fd30de1d1b26407e152e8a15
andmholm <andmholm@iu.edu>
2023-12-26 20:13:56 -0500

    working vjp example

--- tests/experimental.py
+++ tests/experimental.py
@@ -1,6 +1,6 @@
 import torch
-import torch.autograd.forward_ad as fwad
-import torch.autograd.functional as f
+import torch.autograd.functional as tf
+import deepnet.autograd.functional as df
 import numpy as np
 import deepnet
 
@@ -10,17 +10,15 @@ def main():
     b = torch.rand((1,)).float()
     v = torch.ones((1,)).float()
 
-    output, jvp = f.jvp(torch.mul, (a, b), (v, v))
+    output, jvp = tf.jvp(torch.mul, (a, b), (v, v))
     print(output, jvp)
     
-    with deepnet.forward_ad():
-        a = deepnet.rand().float()
-        b = deepnet.rand().float()
-        a.dual(inplace=True)
-        b.dual(inplace=True)
-        c = deepnet.mul(a, b)
-        print(c.tangent) 
+    a = deepnet.tensor(a.numpy())
+    b = deepnet.tensor(b.numpy())
+    v = deepnet.ones((1,))
 
+    output, jvp = df.jvp((a, b), (v, v), deepnet.mul)
+    print(output, jvp)
 
 if __name__ == "__main__":
     main()
