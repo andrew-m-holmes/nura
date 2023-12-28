445bd31f7635d934d6c78be22c383660e03bcd26
andmholm <andmholm@iu.edu>
2023-12-27 22:20:45 -0500

    mul tests passing

--- tests/test_jvp_functional.py
+++ tests/test_jvp_functional.py
@@ -92,6 +92,50 @@ def test_sub_jvp_matrix():
     np.testing.assert_allclose(
         result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
 
+def test_mul_jvp_scalar():
+    a = np.random.rand()
+    b = np.random.rand()
+
+    a_tensor = deepnet.tensor(a).dual(deepnet.tensor(1.))
+    b_tensor = deepnet.tensor(b).dual(deepnet.tensor(1.))
+    result_tensor = f.mul(a_tensor, b_tensor)
+
+    h = 1e-8
+    expected_a = ((a + h) * b - (a - h) * b) / (2 * h)
+    expected_b = (a * (b + h) - a * (b - h)) / (2 * h)
+    expected = expected_a + expected_b
+    np.testing.assert_allclose(
+        result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
+
+def test_mul_jvp_vector():
+    a = np.random.rand(5)
+    b = np.random.rand(5)
+
+    a_tensor = deepnet.tensor(a).dual(deepnet.tensor(np.ones(5)))
+    b_tensor = deepnet.tensor(b).dual(deepnet.tensor(np.ones(5)))
+    result_tensor = f.mul(a_tensor, b_tensor)
+
+    h = 1e-8
+    expected_a = ((a + h) * b - (a - h) * b) / (2 * h)
+    expected_b = (a * (b + h) - a * (b - h)) / (2 * h)
+    expected = expected_a + expected_b
+    np.testing.assert_allclose(
+        result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
+
+def test_mul_jvp_matrix():
+    a = np.random.rand(3, 3)
+    b = np.random.rand(3, 3)
+
+    a_tensor = deepnet.tensor(a).dual(deepnet.tensor(np.ones((3, 3))))
+    b_tensor = deepnet.tensor(b).dual(deepnet.tensor(np.ones((3, 3))))
+    result_tensor = f.mul(a_tensor, b_tensor)
+
+    h = 1e-8
+    expected_a = ((a + h) * b - (a - h) * b) / (2 * h)
+    expected_b = (a * (b + h) - a * (b - h)) / (2 * h)
+    expected = expected_a + expected_b
+    np.testing.assert_allclose(
+        result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
 
 
 def main():
@@ -105,11 +149,19 @@ def main():
         test_add_jvp_vector()
         test_add_jvp_matrix()
 
-        # SUb JVP Tests
+        # Sub JVP Tests
 
         test_sub_jvp_scalar()
         test_sub_jvp_vector()
         test_sub_jvp_matrix()
+
+        # Mul JVP Tests
+
+        test_mul_jvp_scalar()
+        test_mul_jvp_vector()
+        test_mul_jvp_matrix()
+
+
 
         print("All tests passed")
 
