b03b6f182dc8f6ee3b956f72766e224dd74c733b
andmholm <andmholm@iu.edu>
2023-12-27 22:36:40 -0500

    small change

--- tests/experimental.py
+++ tests/experimental.py
@@ -6,36 +6,19 @@ import deepnet
 
 
 def main():
-    a = np.random.rand(1).astype(np.float32)
-    b = np.random.rand(1).astype(np.float32)
-    v = np.ones_like(a)
 
-    a_torch_tensor = torch.from_numpy(a)
-    b_torch_tensor = torch.from_numpy(b)
-    v_torch_tensor = torch.from_numpy(v)
+    a = torch.rand((1,)).float()
+    b = torch.rand((1,)).float()
 
-    a_deepnet_tensor = deepnet.tensor(a)
-    b_deepnet_tensor = deepnet.tensor(b)
-    v_deepnet_tensor = deepnet.tensor(v)
-
-    torch_jvp_out, torch_jvp = taf.jvp(
-        torch.div, (a_torch_tensor, b_torch_tensor), (v_torch_tensor, v_torch_tensor))
-    print(f"Torch results:\nOutput: {torch_jvp_out}\nJVP: {torch_jvp}")
-    print()
-    deepnet_jvp_out, deepnet_jvp = daf.jvp(
-        (a_deepnet_tensor, b_deepnet_tensor), (v_deepnet_tensor, v_deepnet_tensor), deepnet.div)
-    print(f"Deepnet results:\nOutput: {deepnet_jvp_out}\nJVP: {deepnet_jvp}")
-
-    print()
-    torch_jvp_out, torch_jvp = taf.jvp(
-        torch.mul, (a_torch_tensor, b_torch_tensor), (v_torch_tensor, v_torch_tensor))
-    print(f"Torch results:\nOutput: {torch_jvp_out}\nJVP: {torch_jvp}")
-    print()
-    deepnet_jvp_out, deepnet_jvp = daf.jvp(
-        (a_deepnet_tensor, b_deepnet_tensor), (v_deepnet_tensor, v_deepnet_tensor), deepnet.mul)
-    print(f"Deepnet results:\nOutput: {deepnet_jvp_out}\nJVP: {deepnet_jvp}")
-
-
+    v = torch.ones_like(a)
+    output, jvp = taf.jvp(torch.div, (a, b), (v, v))
+    print(output, jvp, sep="\n")
+    
+    a = deepnet.tensor(a.numpy())
+    b = deepnet.tensor(b.numpy())
+    v = deepnet.ones_like(a)
+    output, jvp = daf.jvp((a, b), (v, v), deepnet.div)
+    print(output, jvp, sep="\n")
 
 if __name__ == "__main__":
     main()
