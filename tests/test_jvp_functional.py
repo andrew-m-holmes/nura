c3c9b2cdba1b60d3fbcce1e0ce6a53fdaa3e0f84
andmholm <andmholm@iu.edu>
2023-12-27 22:36:27 -0500

    div tests passing

--- tests/test_jvp_functional.py
+++ tests/test_jvp_functional.py
@@ -137,6 +137,52 @@ def test_mul_jvp_matrix():
     np.testing.assert_allclose(
         result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
 
+def test_div_jvp_scalar():
+    a = np.random.rand()   
+    b = np.random.rand() 
+
+    a_tensor = deepnet.tensor(a).dual(deepnet.tensor(1.))
+    b_tensor = deepnet.tensor(b).dual(deepnet.tensor(1.))
+    result_tensor = f.div(a_tensor, b_tensor)
+
+    h = 1e-8
+    expected_a = ((a + h) / b - (a - h) / b) / (2 * h)
+    expected_b = (a / (b + h) - a / (b - h)) / (2 * h)
+    expected = expected_a + expected_b
+    np.testing.assert_allclose(
+        result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
+
+def test_div_jvp_vector():
+    a = np.random.rand(5) 
+    b = np.random.rand(5) 
+
+    a_tensor = deepnet.tensor(a).dual(deepnet.tensor(np.ones(5)))
+    b_tensor = deepnet.tensor(b).dual(deepnet.tensor(np.ones(5)))
+    result_tensor = f.div(a_tensor, b_tensor)
+
+    h = 1e-8
+    expected_a = ((a + h) / b - (a - h) / b) / (2 * h)
+    expected_b = (a / (b + h) - a / (b - h)) / (2 * h)
+    expected = expected_a + expected_b
+    np.testing.assert_allclose(
+        result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
+    b = np.random.rand(3, 3) 
+
+def test_div_jvp_matrix():
+    a = np.random.rand(3, 3)
+    b = np.random.rand(3, 3)
+
+    a_tensor = deepnet.tensor(a).dual(deepnet.tensor(np.ones((3, 3))))
+    b_tensor = deepnet.tensor(b).dual(deepnet.tensor(np.ones((3, 3))))
+    result_tensor = f.div(a_tensor, b_tensor)
+
+    h = 1e-8
+    expected_a = ((a + h) / b - (a - h) / b) / (2 * h)
+    expected_b = (a / (b + h) - a / (b - h)) / (2 * h)
+    expected = expected_a + expected_b
+    np.testing.assert_allclose(
+        result_tensor.tangent.data, expected, rtol=1e-5, atol=1e-5)
+
 
 def main():
 
@@ -161,6 +207,11 @@ def main():
         test_mul_jvp_vector()
         test_mul_jvp_matrix()
 
+        # Div JVP Tests
+
+        test_div_jvp_scalar()
+        test_div_jvp_vector()
+        test_div_jvp_matrix()
 
 
         print("All tests passed")
