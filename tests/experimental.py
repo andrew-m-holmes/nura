15e369a31e79f90638d00a85ccaf4cf1d290fa2f
andmholm <andmholm@iu.edu>
2024-01-03 22:01:17 -0500

    experimental.py empty

--- tests/experimental.py
+++ tests/experimental.py
@@ -1,21 +1,12 @@
+import numpy as np
 import torch
 import torch.autograd.functional as taf
-import deepnet.autograd.functional as daf
-import numpy as np
 import deepnet
+import deepnet.autograd.functional as daf
 
 
 def main():
-
-    a = deepnet.rand((2, 3, 4), use_grad=True)
-    print(deepnet.is_contiguous(a))
-    b = a[:, 1, 2:3]
-    print(deepnet.is_contiguous(b))
-    c = b.contiguous()
-    print(deepnet.is_contiguous(c))
-    print(c)
-    c.backward(deepnet.ones_like(c))
-    print(b.grad)
+    pass
 
 if __name__ == "__main__":
     main()
