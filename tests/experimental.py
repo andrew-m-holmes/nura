632373ecbf921cddfc099e4408b397e04b593c0d
Andrew <andrewholmes011002@gmail.com>
2024-01-07 17:11:29 -0500

    working pretty good so far

--- tests/experimental.py
+++ tests/experimental.py
@@ -3,15 +3,21 @@ import torch
 import torch.autograd as torch_autograd
 import deepnet
 import deepnet.autograd as deepnet_autograd
+from deepnet.autograd.functional import vjp, jvp, grad, jacobian
 
 
 def main():
     a = torch.tensor(3., requires_grad=True)
     b = torch.tensor(8., requires_grad=True)
-    output = torch.div(a, b)
+    output = torch.sub(torch.mul(torch.div(a, b), a), b)
     df_da, df_db = torch_autograd.grad(output, (a, b))
     print(df_da, df_db)
 
+    a = deepnet.tensor(3., use_grad=True).double()
+    b = deepnet.tensor(8., use_grad=True)
+    output = deepnet.sub(deepnet.mul(deepnet.div(a, b), a), b)
+    df_da, df_db = grad((a, b), output)
+    print(df_da, df_db)
 
 if __name__ == "__main__":
     main()
