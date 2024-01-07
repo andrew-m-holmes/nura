31972becea30f5a60e0085cffe6dd7774a9e57f9
Andrew <andrewholmes011002@gmail.com>
2024-01-07 18:12:16 -0500

    merging to main

--- tests/experimental.py
+++ tests/experimental.py
@@ -7,17 +7,7 @@ from deepnet.autograd.functional import vjp, jvp, grad, jacobian
 
 
 def main():
-    a = torch.tensor(3., requires_grad=True)
-    b = torch.tensor(8., requires_grad=True)
-    output = torch.sub(torch.mul(torch.div(a, b), a), b)
-    df_da, df_db = torch_autograd.grad(output, (a, b))
-    print(df_da, df_db)
-
-    a = deepnet.tensor(3., use_grad=True).double()
-    b = deepnet.tensor(8., use_grad=True)
-    output = deepnet.sub(deepnet.mul(deepnet.div(a, b), a), b)
-    df_da, df_db = grad((a, b), output)
-    print(df_da, df_db)
+    pass
 
 if __name__ == "__main__":
     main()
