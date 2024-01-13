e4cdc0e0b10a458ddce1eb2778fa8c6bac374fb1
Andrew <andrewholmes011002@gmail.com>
2024-01-12 20:30:33 -0500

    error from paper

--- blog/errors.py
+++ blog/errors.py
@@ -7,22 +7,30 @@ def f(x):
 def df(x):
     return 2 * x
 
+def f(x):
+    return 64 * x * (1 - x) * (1 - 2*x)**2 * (1 - 8*x + 8*x**2)**2
+
+def df(x):
+    return (64*x*(1 - 2*x)**2*(1 - x)*(32*x - 16)*(8*x**2 - 8*x + 1) - 
+            64*x*(1 - 2*x)**2*(8*x**2 - 8*x + 1)**2 + 
+            64*x*(1 - x)*(8*x - 4)*(8*x**2 - 8*x + 1)**2 + 
+            64*(1 - 2*x)**2*(1 - x)*(8*x**2 - 8*x + 1)**2)
+
 x0 = 2.
-hs = np.logspace(-7, -1, 150).astype(np.float32)  
+hs = np.logspace(-7, 0, 100).astype(np.float32)  
 
 true_grad = df(x0)
 forward_grad = (f(x0 + hs) - f(x0)) / hs
-# central_grad = (f(x0 + hs) - f(x0 - hs)) / (2 * hs)
-# central_difference_error = abs(true_grad - central_grad)
+central_grad = (f(x0 + hs) - f(x0 - hs)) / (2 * hs)
+central_difference_error = abs(true_grad - central_grad)
 forward_difference_error = abs(true_grad - forward_grad)
 
 plt.style.use("dark_background")
-# plt.loglog(hs, central_difference_error, label="Central Difference")
+plt.loglog(hs, central_difference_error, label="Central Difference")
 plt.loglog(hs, forward_difference_error, label="Forward Difference\nDerivative")
 plt.xlabel("h")
 plt.ylabel("Error")
 plt.axhline(0, color="white")  
 plt.axvline(0, color="white")  
-plt.title("Finite Difference Approximation Error")
 plt.legend()
 plt.show()
