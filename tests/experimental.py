1d9ee6c4e492e979a3e1d119020319aa83316785
Andrew Holmes <andrewholmes011002@gmail.com>
2024-01-23 21:44:51 -0500

    it's a mess but we're getting started

--- tests/experimental.py
+++ tests/experimental.py
@@ -1,19 +1,11 @@
-import numpy as np
-import jax.numpy as jnp
+import numpy as np 
 import deepnet
-import deepnet.autograd as deepnet_autograd
-from deepnet.autograd.functional import vjp, jvp, grad, jacfwd, _get_perturbations
-
 
 def main():
 
-   a = deepnet.rand((4,)) 
-   b = deepnet.rand((4,))
-   out, jac = jacfwd((a, b), deepnet.mul, use_graph=True)
-   out.backward(deepnet.ones_like(out))
-   print(jac)
-   print(a.grad)
-
+    data = np.random.randint(0, 10, (2, 2))
+    a = deepnet.tensor(data, diff=True).float()
+    b = deepnet.tensor(data * -5., diff=True).float()
 
 if __name__ == "__main__":
     main()
