420f7e9d18813f3896353125655991de47ba44eb
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-21 15:10:55 -0500

    why did it not push

--- deepnet/utils.py
+++ deepnet/utils.py
@@ -4,6 +4,8 @@ from deepnet.types import dtype, _dim
 from deepnet.tensors import Tensor
 from typing import Optional, Type, Any, Tuple, Union
 
+#
+
 
 def zeros(
     dim: Union[_dim, int], usegrad=False, dtype: Optional[Type[dtype]] = None
