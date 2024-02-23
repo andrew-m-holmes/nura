f2af529d637ec361dc75b4a85096645d2cdd268e
Andrew Holmes <andrewholmes011002@gmail.com>
2024-02-22 19:15:17 -0500

    utils with new functions and moved atot

--- deepnet/utils.py
+++ deepnet/utils.py
@@ -1,23 +1,22 @@
 import numpy as np
-import deepnet as dn
+import deepnet
 from deepnet.types import dtype, _dim
-from deepnet.tensors import Tensor
+from deepnet.tensors import Tensor, tensor
 from typing import Optional, Type, Any, Tuple, Union
+
 
 def zeros(
     dim: Union[_dim, int], usegrad=False, dtype: Optional[Type[dtype]] = None
 ) -> Tensor:
     dim = todim(dim)
     zero_arr = np.zeros(dim)
-    return dn.tensor(zero_arr, usegrad, dtype)
+    return tensor(zero_arr, usegrad, dtype)
 
 
-def zeroslike(
-    tensor: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None
-) -> Tensor:
-    data = tensor.data
+def zeroslike(a: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None) -> Tensor:
+    data = a.data
     zero_arr = np.zeros_like(data)
-    return dn.tensor(zero_arr, usegrad, dtype)
+    return tensor(zero_arr, usegrad, dtype)
 
 
 def ones(
@@ -25,15 +24,13 @@ def ones(
 ) -> Tensor:
     dim = todim(dim)
     ones_arr = np.ones(dim)
-    return dn.tensor(ones_arr, usegrad, dtype)
+    return tensor(ones_arr, usegrad, dtype)
 
 
-def oneslike(
-    tensor: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None
-) -> Tensor:
-    data = tensor.data
+def oneslike(a: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None) -> Tensor:
+    data = a.data
     ones_arr = np.ones_like(data)
-    return dn.tensor(ones_arr, usegrad, dtype)
+    return tensor(ones_arr, usegrad, dtype)
 
 
 def randn(
@@ -43,13 +40,11 @@ def randn(
 ) -> Tensor:
     dim = todim(dim)
     randn_arr = np.random.randn(*dim)
-    return dn.tensor(randn_arr, usegrad, dtype)
+    return tensor(randn_arr, usegrad, dtype)
 
 
-def randnlike(
-    tensor: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None
-) -> Tensor:
-    dim = tensor.dim
+def randnlike(a: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None) -> Tensor:
+    dim = a.dim
     return randn(dim, usegrad, dtype)
 
 
@@ -60,13 +55,11 @@ def rand(
 ) -> Tensor:
     dim = todim(dim)
     rand_arr = np.random.rand(*dim)
-    return dn.tensor(rand_arr, usegrad, dtype)
+    return tensor(rand_arr, usegrad, dtype)
 
 
-def randlike(
-    tensor: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None
-) -> Tensor:
-    dim = tensor.dim
+def randlike(a: Tensor, usegrad=False, dtype: Optional[Type[dtype]] = None) -> Tensor:
+    dim = a.dim
     return rand(dim, usegrad, dtype)
 
 
@@ -75,19 +68,19 @@ def randint(
 ) -> Tensor:
     dim = todim(dim)
     randint_arr = np.random.randint(low, high, dim)
-    return dn.tensor(randint_arr, dtype=dtype)
+    return tensor(randint_arr, dtype=dtype)
 
 
 def randintlike(
-    low: int, high: int, tensor: Tensor, dtype: Optional[Type[dtype]] = None
+    low: int, high: int, a: Tensor, dtype: Optional[Type[dtype]] = None
 ) -> Tensor:
-    dim = tensor.dim
+    dim = a.dim
     return randint(low, high, dim, dtype)
 
 
 def identity(n: int, usegrad=False, dtype: Optional[Type[dtype]] = None) -> Tensor:
     data = np.identity(n)
-    return dn.tensor(data, usegrad, dtype)
+    return tensor(data, usegrad, dtype)
 
 
 def full(
@@ -98,7 +91,7 @@ def full(
 ) -> Tensor:
     dim = todim(dim)
     data = np.full(dim, num)
-    return dn.tensor(data, usegrad, dtype)
+    return tensor(data, usegrad, dtype)
 
 
 def eye(
@@ -108,17 +101,74 @@ def eye(
     dtype: Optional[Type[dtype]] = None,
 ) -> Tensor:
     data = np.eye(n, m, k)
-    return dn.tensor(data, dtype=dtype)
+    return tensor(data, dtype=dtype)
 
 
-def to(tensor: Tensor, dtype: Type[dtype]):
+def any(a: Tensor, dim: Optional[_dim] = None, keepdims=False):
+    return tensor(np.any(a.data, axis=dim, keepdims=keepdims))
+
+
+def all(a: Tensor, dim: Optional[_dim] = None, keepdims=False):
+    return tensor(np.all(a.data, axis=dim, keepdims=keepdims))
+
+
+def hashtensor(a: Tensor) -> int:
+    return hash(id(a))
+
+
+def equal(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = deepnet.atot(a, b)
+    return tensor(np.equal(a.data, b.data))
+
+
+def less(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = atot(a, b)
+    return tensor(np.less(a.data, b.data))
+
+
+def lesseq(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = atot(a, b)
+    return tensor(np.less_equal(a.data, b.data))
+
+
+def greater(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = atot(a, b)
+    return tensor(np.greater(a.data, b.data))
+
+
+def greatereq(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = atot(a, b)
+    return tensor(np.greater_equal(a.data, b.data))
+
+
+def notequal(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = atot(a, b)
+    return tensor(np.not_equal(a.data, b.data))
+
+
+def tensorand(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = atot(a, b)
+    return tensor(a.data and b.data)
+
+
+def tensoror(a: Union[Tensor, Any], b: Union[Tensor, Any]) -> Tensor:
+    a, b = atot(a, b)
+    return tensor(a.data or b.data)
+
+
+def tensornot(a: Union[Tensor, Any]) -> Tensor:
+    b = atot(a)[0]
+    return tensor(not b.data)
+
+
+def atot(*args: Any) -> Union[Tuple[Tensor, ...], Tensor]:
+    return tuple(a if istensor(a) else tensor(a) for a in args)
+
+
+def to(a: Tensor, dtype: Type[dtype]):
     assert istensor(tensor)
-    data = dtype.numpy(tensor.data)
-    return dn.tensor(data, tensor.usegrad, dtype)
-
-
-def hashtensor(tensor: Tensor) -> int:
-    return hash(id(tensor))
+    data = dtype.numpy(a.data)
+    return tensor(data, a.usegrad, dtype)
 
 
 def todim(dim: Any) -> Tuple[int, ...]:
@@ -129,13 +179,13 @@ def todim(dim: Any) -> Tuple[int, ...]:
     return dim
 
 
-def iscontig(tensor: Tensor) -> bool:
-    return tensor.data.flags["C_CONTIGUOUS"]
+def iscontig(a: Tensor) -> bool:
+    return a.data.flags["C_CONTIGUOUS"]
 
 
 def istensor(obj: Any) -> bool:
     return isinstance(obj, Tensor)
 
 
-def typename(tensor: Tensor) -> str:
-    return tensor.__class__.__name__
+def typename(a: Tensor) -> str:
+    return a.__class__.__name__
