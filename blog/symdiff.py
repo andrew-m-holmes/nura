27e2ac905fefd3391fb81dda109256a81fdd0da8
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-02 15:41:53 -0500

    update to blog

--- blog/symdiff.py
+++ blog/symdiff.py
@@ -1,6 +1,20 @@
-from sympy import *
+from sympy import symbols, cos, exp, diff
 
-x = symbols("x")
-fog = 4 * (cos(x) + 2 * x - exp(x)) ** 2
-df_dx = diff(fog, x)
-print(df_dx)
+
+def main():
+    x = symbols("x")
+    fog = 4 * (cos(x) + 2 * x - exp(x)) ** 2
+    dfdx = diff(fog, x)
+    print(dfdx)
+
+    def f(x):
+        if x > 2:
+            return x * 2 + 5
+        return x / 2 + 5
+
+    dfdx = diff(f)
+    print(dfdx)
+
+
+if __name__ == "__main__":
+    main()
