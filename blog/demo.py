28ea016a1e4e89a0d1cfb163d85d269ea11cdce4
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-10 15:51:15 -0400

    updated demo

--- blog/demo.py
+++ blog/demo.py
@@ -7,7 +7,7 @@ def fn(a, b, c):
 a = nura.tensor([1.0, 2.0, 3.0, 4.0])
 b = nura.tensor([5.0, 6.0, 7.0, 8.0])
 c = nura.tensor(1.0)
-r = nura.ones(4).float()
+r = nura.ones(4).double()
 
 output, jacobian = jacfwd((a, b, c), fn, pos=1)
 print(f"output:\n{output}\n\njacobian:\n{jacobian}")
