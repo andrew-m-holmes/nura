8311544ff38dc916ff308c5a0b631e1f6c0b4825
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-17 17:39:38 -0400

    tensors only, better gelu and attn

--- nura/nn/functional.py
+++ nura/nn/functional.py
@@ -1,12 +1,12 @@
 import nura.functional as f
 import nura.nn.functions as fn
 from nura.tensors import Tensor
-from nura.utils import atot, where
+from nura.utils import where
 from typing import Optional, Tuple
 
 
 def linear(x: Tensor, w: Tensor, b: Optional[Tensor] = None):
-    out = f.matmul(x, w.transpose())
+    out = f.matmul(x, w.T)
     if b is not None:
         out = out + b
     return out
@@ -25,33 +25,30 @@ def tanh(z: Tensor):
 
 
 def relu(z: Tensor):
-    z = atot(z)[0]
     out = fn._ReLU.apply(z)
     return out
 
 
 def relu6(z: Tensor):
-    z = atot(z)[0]
     out = fn._ReLU6.apply(z)
     return out
 
 
 def leakyrelu(z: Tensor, slope=0.01):
-    z = atot(z)[0]
     out = fn._LeakyReLU.apply(z, slope)
     return out
 
 
 def elu(z: Tensor, alpha=1.0):
-    z = atot(z)[0]
     out = fn._ELU.apply(z, alpha)
     return out
 
 
 def gelu(z: Tensor):
-    z = atot(z)[0]
     piconst = 0.79788456
-    out = 0.5 * z * (1.0 + tanh(piconst * (z + 0.044715 * f.pow(z, 3.0))))
+    const = 0.044715
+    inner = piconst * (z + const * (z * z * z))  # avoid invalid log in pow backward
+    out = 0.5 * z * (1 + tanh(inner))
     return out
 
 
@@ -64,7 +61,7 @@ def softmax(a: Tensor, dim=-1):
 def selfattention(
     q: Tensor, k: Tensor, v: Tensor, mask: Optional[Tensor] = None
 ) -> Tuple[Tensor, Tensor]:
-    scaled = f.matmul(q, k.T) / f.sqrt(k.dim[-1])
+    scaled = f.matmul(q, k.T) / k.dim[-1] ** 0.5
     if mask is not None:
         scaled = where(mask == True, -1e-9, scaled)
     attn = softmax(scaled, dim=-1)
