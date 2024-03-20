7d1c63370e033bf69efb393ce2649655c8c102cf
Andrew Holmes <andrewholmes011002@gmail.com>
2024-03-20 12:40:17 -0400

    to makes shallow copy and print updated

--- tests/experimental.py
+++ tests/experimental.py
@@ -1,7 +1,5 @@
 import nura
 import nura.nn as nn
-import torch
-import torch.nn.functional as f
 import numpy as np
 
 
@@ -16,40 +14,14 @@ def main():
         def forward(self, x):
             return self.linear(x)
 
-    bsize, seqlen, dm = 2, 4, 3
-
-    q = nura.randn(bsize, seqlen, dm)
-    k = q.clone()
-    v = q.clone()
-
-    mask = nura.tri(seqlen, seqlen).bool()
-    ctx, attn = nn.selfattention(q, k, v, -1, mask)
-    print(f"nura attn:\n{attn}")
-    print(f"nura ctx:\n{ctx}")
-
-    print("-" * 50)
-
-    s = nura.matmul(q, k.transpose(-1, -2)) / dm**0.5
-    s = nura.where(mask, s, -nura.inf)
-    attn = nn.softmax(s, dim=-1)
-    print(f"nura attn:\n{attn}")
-    ctx = nura.matmul(attn, v)
-    print(f"nura ctx:\n{ctx}")
-
-    print("-" * 50)
-
-    q = torch.from_numpy(q.data)
-    k = q.clone()
-    v = q.clone()
-
-    s = torch.matmul(q, k.transpose(-1, -2)) / dm**0.5
-    ones = torch.ones((seqlen, seqlen)).bool()
-    mask = torch.tril(ones)
-    s = s.masked_fill(mask == False, -1e9)
-    attn = f.softmax(s, dim=-1)
-    print(f"torch attn:\n{attn}")
-    ctx = torch.matmul(attn, v)
-    print(f"torch ctx:\n{ctx}")
+    model = Model()
+    modelhf = model.half()
+    modelfl = model.float()
+    modeldf = model.double()
+    print(model, modelhf, modelfl, modeldf, sep="\n\n")
+    print(modelhf is modelfl)
+    print(modelhf is modeldf)
+    print(modelhf is model)
 
 
 if __name__ == "__main__":
