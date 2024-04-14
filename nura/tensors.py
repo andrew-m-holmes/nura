3dafbd99a66a452dcf0f358ff77289cf605cb2ca
Andrew Holmes <andrewholmes011002@gmail.com>
2024-04-14 15:08:42 -0400

    type hints

--- nura/tensors.py
+++ nura/tensors.py
@@ -1,8 +1,8 @@
 import nura
 import nura.types as types
-from nura.types import Tensorlike, dimlike, dim, dtype
+from nura.types import Tensorlike, Scalar, dtype, dim, dimlike, pyint
 from nura.autograd.graph import Node
-from typing import Optional, Type, Any, Union
+from typing import Optional, Type, Any, Union, List
 from numpy import ndarray
 
 
@@ -16,7 +16,6 @@ class Tensor:
         backfn: Optional[Node],
         leaf: bool,
     ) -> None:
-
         self._data: ndarray = data
         self._grad: Optional[Tensor] = grad
         self._backfn: Optional[Node] = backfn
@@ -64,235 +63,241 @@ class Tensor:
         return self.dtype in (types.half, types.float, types.double)
 
     @property
-    def T(self):
+    def T(self) -> "Tensor":
         revdims = tuple(range(self.ndim - 1, -1, -1))
         return self.permute(revdims)
 
-    def item(self):
+    def item(self) -> Scalar:
         if self.nelem != 1:
             raise RuntimeError(
                 f"Cannot retrieve a single element from a Tensor with {self.nelem} elements"
             )
         return self.data.item()
 
-    def list(self):
+    def list(self) -> List[Any]:
         return self.data.tolist()
 
     def to(self, dtype: Type[types.dtype]):
         return nura.to(self, dtype)
 
-    def byte(self):
+    def byte(self) -> "Tensor":
         return self.to(types.byte)
 
-    def char(self):
+    def char(self) -> "Tensor":
         return self.to(types.char)
 
-    def short(self):
+    def short(self) -> "Tensor":
         return self.to(types.short)
 
-    def int(self):
+    def int(self) -> "Tensor":
         return self.to(types.int)
 
-    def long(self):
+    def long(self) -> "Tensor":
         return self.to(types.long)
 
-    def half(self):
+    def half(self) -> "Tensor":
         return self.to(types.half)
 
-    def float(self):
+    def float(self) -> "Tensor":
         return self.to(types.float)
 
-    def double(self):
+    def double(self) -> "Tensor":
         return self.to(types.double)
 
-    def bool(self):
+    def bool(self) -> "Tensor":
         return self.to(types.bool)
 
-    def backward(self, grad: Optional["Tensor"] = None):
+    def backward(self, grad: Optional["Tensor"] = None) -> None:
         nura.backward(self, grad)
 
-    def cleargrad(self):
+    def cleargrad(self) -> None:
         self._grad = None
 
-    def clearedgrad(self):
+    def clearedgrad(self) -> "Tensor":
         cls = type(self)
         return cls(self.data, self.usegrad, None, self.backfn, self.leaf)
 
-    def zerograd(self):
+    def zerograd(self) -> None:
         self._grad = nura.zeroslike(self)
 
-    def zeroedgrad(self):
+    def zeroedgrad(self) -> "Tensor":
         cls = type(self)
         return cls(self.data, self.usegrad, nura.zeroslike(self), None, True)
 
-    def usesgrad(self):
+    def usesgrad(self) -> None:
         self._usegrad = True
 
-    def usedgrad(self):
+    def usedgrad(self) -> "Tensor":
         cls = type(self)
         return cls(self.data, True, self.grad, self.backfn, self.leaf)
 
-    def mutate(self, **attrs: Any):
+    def mutate(self, **attrs: Any) -> None:
         for k, v in attrs.items():
             setattr(self, f"_{k}", v)
 
-    def mutated(self, **attrs: Any):
+    def mutated(self, **attrs: Any) -> "Tensor":
         cls = type(self)
         t = cls(self.data, self.usegrad, self.grad, self.backfn, self.leaf)
         for k, v in attrs.items():
             setattr(t, f"_{k}", v)
         return t
 
-    def nograd(self):
-        self._usegrad = False
-
-    def detach(self):
+    def detach(self) -> "Tensor":
         return nura.detach(self)
 
-    def clone(self):
+    def clone(self) -> "Tensor":
         return nura.clone(self)
 
-    def contiguous(self):
+    def contiguous(self) -> "Tensor":
         return nura.tocontiguous(self)
 
-    def exp(self):
+    def exp(self) -> "Tensor":
         return nura.exp(self)
 
-    def sum(self, dim: Optional[dimlike] = None, keepdims=False):
+    def sum(self, dim: Optional[dimlike] = None, keepdims=False) -> "Tensor":
         return nura.sum(self, dim, keepdims)
 
-    def max(self, dim: Optional[dimlike] = None, keepdims=False):
+    def max(self, dim: Optional[dimlike] = None, keepdims=False) -> "Tensor":
         return nura.max(self, dim, keepdims)
 
-    def min(self, dim: Optional[dimlike] = None, keepdims=False):
+    def min(self, dim: Optional[dimlike] = None, keepdims=False) -> "Tensor":
         return nura.min(self, dim, keepdims)
 
-    def squeeze(self, dim: Optional[dimlike] = None):
+    def squeeze(self, dim: Optional[dimlike] = None) -> "Tensor":
         return nura.squeeze(self, dim)
 
-    def unsqueeze(self, dim: dimlike):
+    def unsqueeze(self, dim: dimlike) -> "Tensor":
         return nura.unsqueeze(self, dim)
 
-    def view(self, newdim: types.dim):
+    def view(self, newdim: types.dim) -> "Tensor":
         return nura.view(self, newdim)
 
-    def reshape(self, newdim: types.dim):
+    def reshape(self, newdim: types.dim) -> "Tensor":
         return nura.reshape(self, newdim)
 
-    def transpose(self, dim0=-2, dim1=-1):
+    def transpose(self, dim0: pyint = -2, dim1: pyint = -1) -> "Tensor":
         return nura.transpose(self, dim0, dim1)
 
-    def permute(self, dims: types.dim):
+    def permute(self, dims: types.dim) -> "Tensor":
         return nura.permute(self, dims)
 
-    def any(self, dim: Optional[dimlike] = None, keepdims=False):
+    def any(self, dim: Optional[dimlike] = None, keepdims=False) -> "Tensor":
         return nura.tensorany(self, dim, keepdims)
 
-    def all(self, dim: Optional[dimlike] = None, keepdims=False):
+    def all(self, dim: Optional[dimlike] = None, keepdims=False) -> "Tensor":
         return nura.tensorall(self, dim, keepdims)
 
-    def __add__(self, other):
+    def __add__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.add(self, other)
 
-    def __radd__(self, other):
+    def __radd__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.add(self, other)
 
-    def __iadd__(self, other):
+    def __iadd__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.iadd(self, other)
 
-    def __sub__(self, other):
+    def __sub__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.sub(self, other)
 
-    def __rsub__(self, other):
+    def __rsub__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.sub(tensor(other, dtype=self.dtype), self)
 
-    def __isub__(self, other):
+    def __isub__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.isub(self, other)
 
-    def __mul__(self, other):
+    def __mul__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.mul(self, other)
 
-    def __rmul__(self, other):
+    def __rmul__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.mul(self, other)
 
-    def __imul__(self, other):
+    def __imul__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.imul(self, other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.div(self, other)
 
-    def __rtruediv__(self, other):
+    def __rtruediv__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.div(tensor(other, dtype=self.dtype), self)
 
-    def __itruediv__(self, other):
+    def __itruediv__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.idiv(self, other)
 
-    def __matmul__(self, other):
+    def __floordiv__(self, other: Union["Tensor", Scalar]) -> "Tensor":
+        return nura.floordiv(self, other)
+
+    def __rfloordiv__(self, other: Union["Tensor", Scalar]) -> "Tensor":
+        return nura.floordiv(tensor(other, dtype=self.dtype), self)
+
+    def __ifloordiv__(self, other: Union["Tensor", Scalar]) -> "Tensor":
+        return nura.ifloordiv(self, other)
+
+    def __matmul__(self, other: "Tensor") -> "Tensor":
         return nura.matmul(self, other)
 
-    def __imatmul__(self, other):
+    def __imatmul__(self, other: "Tensor") -> "Tensor":
         return nura.imatmul(self, other)
 
-    def __pow__(self, other):
+    def __pow__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.pow(self, other)
 
-    def __ipow__(self, other):
+    def __ipow__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.ipow(self, other)
 
-    def __rpow__(self, other):
+    def __rpow__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.pow(tensor(other, dtype=self.dtype), self)
 
-    def __pos__(self):
+    def __pos__(self) -> "Tensor":
         return nura.pos(self)
 
-    def __neg__(self):
+    def __neg__(self) -> "Tensor":
         return nura.neg(self)
 
-    def __abs__(self):
+    def __abs__(self) -> "Tensor":
         return nura.abs(self)
 
-    def __invert__(self):
+    def __invert__(self) -> "Tensor":
         return nura.tensornot(self)
 
-    def __eq__(self, other):
+    def __eq__(self, other) -> "Tensor":
         return nura.equal(self, other)
 
-    def __lt__(self, other):
+    def __lt__(self, other) -> "Tensor":
         return nura.less(self, other)
 
-    def __le__(self, other):
+    def __le__(self, other) -> "Tensor":
         return nura.lesseq(self, other)
 
-    def __gt__(self, other):
+    def __gt__(self, other) -> "Tensor":
         return nura.greater(self, other)
 
-    def __ge__(self, other):
+    def __ge__(self, other) -> "Tensor":
         return nura.greatereq(self, other)
 
-    def __ne__(self, other):
+    def __ne__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.notequal(self, other)
 
-    def __hash__(self):
+    def __hash__(self) -> pyint:
         return nura.hashtensor(self)
 
-    def __len__(self):
+    def __len__(self) -> pyint:
         return len(self.data)
 
-    def __bool__(self):
+    def __bool__(self) -> None:
         raise ValueError(
             "Truth of Tensor is undefined for more than one element, use .any() or .all()"
         )
 
-    def __and__(self, other):
+    def __and__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.tensorand(self, other)
 
-    def __or__(self, other):
+    def __or__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.tensoror(self, other)
 
-    def __xor__(self, other):
+    def __xor__(self, other: Union["Tensor", Scalar]) -> "Tensor":
         return nura.tensorxor(self, other)
 
-    def __setattr__(self, name, value):
+    def __setattr__(self, name: str, value: Any) -> None:
         validattrs = (
             "_data",
             "_usegrad",
@@ -309,10 +314,10 @@ class Tensor:
             )
         self.__dict__[name] = value
 
-    def __getitem__(self, slc):
+    def __getitem__(self, slc: Any) -> "Tensor":
         return nura.slice(self, slc)
 
-    def __setitem__(self, slc, item):
+    def __setitem__(self, slc: Any, item: Any) -> None:
         if isinstance(slc, tuple):
             slc = tuple(i.data if isinstance(i, Tensor) else i for i in slc)
         if isinstance(slc, Tensor):
