676ad979cf3ef4b6aa6c7b255948d52a37a32f7a
Andrew Holmes <andrewholmes011002@gmail.com>
2024-04-14 20:24:56 -0400

    div backward

--- tests/test_backward_functional.py
+++ tests/test_backward_functional.py
@@ -489,3 +489,165 @@ def test_mul_different_types_reversed_backward():
     np.testing.assert_allclose(
         a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
     )
+
+
+def test_div_scalar_backward():
+    a, b = np.array(6.0), np.array(2.0)
+    a_tensor = nura.tensor(a, usegrad=True)
+    b_tensor = nura.tensor(b, usegrad=True)
+    result_tensor = f.div(a_tensor, b_tensor)
+    result_tensor.backward(nura.oneslike(result_tensor))
+    h = 1e-8
+
+    def func(x, y):
+        return x / y
+
+    expected_grad_a = (func(a + h, b) - func(a - h, b)) / (2 * h)
+    expected_grad_b = (func(a, b + h) - func(a, b - h)) / (2 * h)
+
+    assert a_tensor.grad is not None
+    assert b_tensor.grad is not None
+    np.testing.assert_allclose(
+        a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
+    )
+    np.testing.assert_allclose(
+        b_tensor.grad.data, expected_grad_b, rtol=1e-7, atol=1e-7
+    )
+
+
+def test_div_vector_backward():
+    a = np.random.rand(5) + 1e-8
+    b = np.random.rand(5) + 1e-8
+    a_tensor = nura.tensor(a, usegrad=True)
+    b_tensor = nura.tensor(b, usegrad=True)
+    result_tensor = f.div(a_tensor, b_tensor)
+    result_tensor.backward(nura.oneslike(result_tensor))
+    h = 1e-8
+
+    def func(x, y):
+        return x / y
+
+    expected_grad_a = (func(a + h, b) - func(a - h, b)) / (2 * h)
+    expected_grad_b = (func(a, b + h) - func(a, b - h)) / (2 * h)
+
+    assert a_tensor.grad is not None
+    assert b_tensor.grad is not None
+    np.testing.assert_allclose(
+        a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
+    )
+    np.testing.assert_allclose(
+        b_tensor.grad.data, expected_grad_b, rtol=1e-7, atol=1e-7
+    )
+
+
+def test_div_matrix_backward():
+    a = np.random.rand(3, 4) + 1e-8
+    b = np.random.rand(3, 4) + 1e-8
+    a_tensor = nura.tensor(a, usegrad=True)
+    b_tensor = nura.tensor(b, usegrad=True)
+    result_tensor = f.div(a_tensor, b_tensor)
+    result_tensor.backward(nura.oneslike(result_tensor))
+    h = 1e-8
+
+    def func(x, y):
+        return x / y
+
+    expected_grad_a = (func(a + h, b) - func(a - h, b)) / (2 * h)
+    expected_grad_b = (func(a, b + h) - func(a, b - h)) / (2 * h)
+
+    assert a_tensor.grad is not None
+    assert b_tensor.grad is not None
+    np.testing.assert_allclose(
+        a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
+    )
+    np.testing.assert_allclose(
+        b_tensor.grad.data, expected_grad_b, rtol=1e-7, atol=1e-7
+    )
+
+
+def test_div_tensor_backward():
+    a = np.random.rand(2, 4, 3) + 1e-8
+    b = np.random.rand(2, 4, 3) + 1e-8
+    a_tensor = nura.tensor(a, usegrad=True)
+    b_tensor = nura.tensor(b, usegrad=True)
+    result_tensor = f.div(a_tensor, b_tensor)
+    result_tensor.backward(nura.oneslike(result_tensor))
+    h = 1e-8
+
+    def func(x, y):
+        return x / y
+
+    expected_grad_a = (func(a + h, b) - func(a - h, b)) / (2 * h)
+    expected_grad_b = (func(a, b + h) - func(a, b - h)) / (2 * h)
+
+    assert a_tensor.grad is not None
+    assert b_tensor.grad is not None
+    np.testing.assert_allclose(
+        a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
+    )
+    np.testing.assert_allclose(
+        b_tensor.grad.data, expected_grad_b, rtol=1e-7, atol=1e-7
+    )
+
+
+def test_div_broadcast_backward():
+    a = np.random.rand(4, 3, 2) + 1e-8
+    b = np.random.rand(3, 1) + 1e-8
+    a_tensor = nura.tensor(a, usegrad=True)
+    b_tensor = nura.tensor(b, usegrad=True)
+    result_tensor = f.div(a_tensor, b_tensor)
+    result_tensor.backward(nura.oneslike(result_tensor))
+    h = 1e-8
+
+    def func(x, y):
+        return x / y
+
+    expected_grad_a = (func(a + h, b) - func(a - h, b)) / (2 * h)
+    expected_grad_b = np.sum(
+        (func(a, b + h) - func(a, b - h)) / (2 * h), axis=(0, 2)
+    ).reshape(b.shape)
+
+    assert a_tensor.grad is not None
+    assert b_tensor.grad is not None
+    np.testing.assert_allclose(
+        a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
+    )
+    np.testing.assert_allclose(
+        b_tensor.grad.data, expected_grad_b, rtol=1e-7, atol=1e-7
+    )
+
+
+def test_div_different_types_backward():
+    a = np.random.rand(4) + 1e-8
+    a_tensor = nura.tensor(a, usegrad=True)
+    result_tensor = nura.div(a_tensor, 2)
+    result_tensor.backward(nura.oneslike(result_tensor))
+    h = 1e-8
+
+    def func(x):
+        return x / 2
+
+    expected_grad_a = (func(a + h) - func(a - h)) / (2 * h)
+
+    assert a_tensor.grad is not None
+    np.testing.assert_allclose(
+        a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
+    )
+
+
+def test_div_different_types_reversed_backward():
+    a = np.random.rand(4) + 1e-8
+    a_tensor = nura.tensor(a, usegrad=True)
+    result_tensor = 2 / a_tensor
+    result_tensor.backward(nura.oneslike(result_tensor))
+    h = 1e-8
+
+    def func(x):
+        return 2 / x
+
+    expected_grad_a = (func(a + h) - func(a - h)) / (2 * h)
+
+    assert a_tensor.grad is not None
+    np.testing.assert_allclose(
+        a_tensor.grad.data, expected_grad_a, rtol=1e-7, atol=1e-7
+    )
