6a81f12a938981645eec899f55394fd7edd76dfa
Andrew Holmes <andrewholmes011002@gmail.com>
2024-04-15 21:25:25 -0400

    just seeing stuff

--- tests/experimental.py
+++ tests/experimental.py
@@ -6,17 +6,19 @@ import numpy as np
 
 def main():
 
-    def rmse(a, y):
-        return np.sqrt(np.mean(np.square(a - y)))
+    def mse(a, y):
+        return np.mean(0.5 * np.square(a - y))
 
     a, y = np.random.rand(4), np.random.randint(0, 2, size=4).astype(float)
-    error = rmse(a, y)
+    error = mse(a, y)
     print(error)
 
     a = nura.tensor(a)
     y = nura.tensor(y)
-    error = f.rmse(a, y)
+    error = f.mse(a, y)
     print(error)
+
+    print(nura.sum(nura.square(a - y)) / a.nelem)
 
 
 if __name__ == "__main__":
