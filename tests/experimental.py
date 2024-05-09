36113295e6cdfd179ecae7568631f656e77ddff4
Andrew Holmes <andrewholmes011002@gmail.com>
2024-05-09 09:06:36 -0400

    dry test

--- tests/experimental.py
+++ tests/experimental.py
@@ -3,18 +3,34 @@ import nura
 import nura.nn as nn
 
 
+class Model(nn.Module):
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.linear1 = nn.Linear(784, 128)
+        self.relu1 = nn.ReLU()
+        self.linear2 = nn.Linear(128, 256)
+        self.relu2 = nn.ReLU()
+        self.linear3 = nn.Linear(256, 10)
+        self.softmax = nn.Softmax(dim=-1)
+
+    def forward(self, x):
+        x = self.relu1(self.linear1(x))
+        x = self.relu2(self.linear2(x))
+        x = self.softmax(self.linear3(x))
+        return x
+
+
 def main():
 
-    a = nura.tensor(3.0, usegrad=True)
-    b = nura.tensor(4.0, usegrad=True)
-    c = a * b
-    c += 1
-    d = a - c
-    c += 1
-    d.backward()
-    e = d * 15.0
-    e.backward(input=c)
-    print(c.grad)
+    model = Model()
+    x = nura.randn(64, 784)
+    y = nura.randint(0, 10, (64,))
+    output = model(x)
+    objective = nn.CrossEntropy(ignoreid=5)
+    loss = objective(output, y)
+    print(loss)
+    loss.backward()
 
 
 if __name__ == "__main__":
