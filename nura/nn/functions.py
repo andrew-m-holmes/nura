fix* for softmax backward and tangent

nura/nn/functions.py
@@ -64,32 +64,40 @@ class Softmax(Function):
         p = context.p
         dim = context.dim
         outshape = p.shape
+
         if p.ndim == 1:
             diagonal = np.diagflat(p)
             offdiagonal = np.outer(p, p)
+            gdata = grad.data
         else:
             p = p.reshape(-1, p.shape[dim])
             diagonal = np.einsum("ij,jk->ijk", p, np.eye(p.shape[dim]))
             offdiagonal = np.einsum("ij,ik->ijk", p, p)
+            gdata = grad.data.reshape(-1, p.shape[dim], 1)
 
         jac = diagonal - offdiagonal
-        # TODO fix (should be matrix vector product)
-        return jac.sum(axis=-1).reshape(outshape) * grad.data
+        gout = np.matmul(jac, gdata)
+        return gout.reshape(outshape) if p.ndim > 1 else gout
 
     @staticmethod
     def tangent(context: Context, grad: Tensor):
         p = context.p
         dim = context.dim
         outshape = p.shape
+
         if p.ndim == 1:
             diagonal = np.diagflat(p)
             offdiagonal = np.outer(p, p)
+            gdata = grad.data
         else:
             p = p.reshape(-1, p.shape[dim])
             diagonal = np.einsum("ij,jk->ijk", p, np.eye(p.shape[dim]))
             offdiagonal = np.einsum("ij,ik->ijk", p, p)
+            gdata = grad.data.reshape(-1, p.shape[dim], 1)
+
         jac = diagonal - offdiagonal
-        return jac.sum(axis=-1).reshape(outshape) * grad.data
+        gout = np.matmul(jac, gdata)
+        return gout.reshape(outshape) if p.ndim > 1 else gout
 
 
 class LogSoftmax(Function):
@@ -101,6 +109,11 @@ class LogSoftmax(Function):
         logsum = np.log(np.exp(x.data - xmax).sum(axis=dim, keepdims=True))
         nll = np.negative(x.data - xmax - logsum)
         return nll
+
+    @staticmethod
+    def backward(context: Context, grad: Tensor):
+        # TODO impl backward
+        raise NotImplementedError
 
 
 class ReLU(Function):

test/experimental.py
@@ -4,34 +4,13 @@ import nura.nn.functional as f
 import numpy as np
 
 
-class Model(nn.Module):
-
-    def __init__(self) -> None:
-        super().__init__()
-        self.linear1 = nn.Linear(128, 64)
-        self.norm = nn.LayerNorm(64)
-        self.linear2 = nn.Linear(64, 10)
-
-    def forward(self, x):
-        x = f.relu(self.norm(self.linear1(x)))
-        return self.linear2(x)
-
-
 def main():
 
-    model = Model()
-    lossfn = nn.CrossEntropy()
-    optimizer = nn.AdaGrad(model.parameters(), learnrate=1.0)
-
-    inputs = nura.randn(2, 128) * 0.1
-    labels = nura.randint(0, 10, 2)
-
-    outputs = model(inputs)
-    loss = lossfn(outputs, labels)
+    x = nura.randint(0, 100, 7).float().attach()
+    y = f.softmax(x, dim=-1)
+    loss = y.sum()
     loss.backward()
-
-    optimizer.step()
-    print(optimizer)
+    print(x.grad)
 
 
 if __name__ == "__main__":